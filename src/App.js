92f5337b21dace17961b881955c14a7ab283d2ff
Steven-C1998 <stevengo123@gmail.com>
2022-01-18 16:55:35 -0500

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,42 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { Component, useEffect } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Table from './components/Table'
 
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {
+    this.state = ({
       //default table values
       rows: 0,
-      columns: 0,
-      color: "white"
-    }
+      columns: 1,
+      color: "black",
+      isFilled: false,
+      mouseDown: false
+    });
   }
+
+  //Functions for buttons
+  addRows = () => {
+    console.log(this.color)
+    this.setState(prev => ({ rows: prev.rows + 1 }))
+
+  }
+
+  addColumns = () => {
+    this.setState(prev => ({ columns: prev.columns + 1 }))
+  }
+
+  deleteRow = () => {
+
+    this.setState(prev => ({ rows: prev.rows - 1 }))
+
+  }
+
+  deleteColumn = () => {
+
+    this.setState(prev => ({ columns: prev.columns - 1 }))
+
+  }
+
+  changeColor = (e) => {
+    console.log(this.columns)
+    let modifiedColor = e.target.value
+    console.log(e.target.value)
+    this.setState({ color: modifiedColor })
+
+  }
+
+  fillCells = () => {
+    this.setState({ isFilled: true })
+  }
+
+  
 
   render() {
     return (
-      <div id="body">
+      <div id="body" onMouseDown={this.mouseDrag} onMouseUp={this.mouseDrag}>
         <div id="all-btns">
-          <button id="rowBtn" class="btns"> Add Row</button>
-          <button id="colBtn" class="btns">Add Column</button>
-          <button id="delRow" class="btns">Delete Row</button>
-          <button id="delCol" class="btns">Delete Column</button>
-          <button id="fillBtn" class="btns">Fill all</button>
-          <button id="resetBtn" class="btns">Reset Color</button>
-          <button id="fillEmpty" class="btns">Fill Empty</button>
+          <button onClick={this.addRows} id="rowBtn" className="btns"> Add Row</button>
+          <button onClick={this.addColumns} id="colBtn" className="btns">Add Column</button>
+          <button onClick={this.deleteRow} id="delRow" className="btns">Delete Row</button>
+          <button onClick={this.deleteColumn} id="delCol" className="btns">Delete Column</button>
+          <button id="fillBtn" onClick={this.fillCells} className="btns">Fill all</button>
+          <button id="resetBtn" className="btns">Reset Color</button>
+          <button id="fillEmpty" className="btns">Fill Empty</button>
           <form>
             <label id="dropDown">Choose a Color</label>
-            <input type="color" id="colorInput" />
+            <input type="color" id="colorInput" onChange={this.changeColor.bind(this)} />
           </form>
         </div>
 
         <div id="container" draggable="false">
-          <table id="mytab1" draggable="false">
-            <tbody draggable="false" id="box">
-            </tbody>
-          </table>
+
+          <Table className="box" state={this.state}>
+
+          </Table>
         </div>
 
       </div>
